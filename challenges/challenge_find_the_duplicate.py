878e2724246ddfccaae60b0706519b991e8fa1b2
jonnoliveira <jonnutri@gmail.com>
2023-06-01 20:21:20 -0300

    Add: Requisito 5 - Find_the_duplicate

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,35 @@
-def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+def find_duplicate(numbers):
+    duplicated_numbers = count_duplicate(numbers)
+
+    # verifica se a lista é um dicionário ou booleano
+    if not duplicated_numbers:
+        return False
+
+    # percorrendo o dicionário (objeto) de números duplicados
+    for number, count in count_duplicate(numbers).items():
+        # verificando se o número se repete mais de uma vez
+        if count > 1:
+            return number
+
+    return False
+
+
+def count_duplicate(numbers):
+    duplicated_number = {}
+
+    # percorrendo a lista de números
+    for number in numbers:
+        # verifica se o número é um inteiro e se é maior que 0
+        if not isinstance(number, int) or number < 0:
+            return False
+
+        # verifica se o número já existe no dicionário (objeto)
+        if number in duplicated_number:
+            duplicated_number[number] += 1
+        else:
+            duplicated_number[number] = 1
+
+    return duplicated_number
+
+
+print(find_duplicate([2, 3, 1, 3, 3]))
