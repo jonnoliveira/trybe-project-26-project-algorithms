416e18e107832b34a4a3b149651da9f579a15db7
jonnoliveira <jonnutri@gmail.com>
2023-06-01 15:46:26 -0300

    Add: Requisito 1 - Study_Schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,22 @@
+# permanence_period = [(1, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)]
+
+
+def is_integer(time):
+    return isinstance(time[0], int) and isinstance(time[1], int)
+
+
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    sum = 0
+    if isinstance(target_time, int) == False:
+        return None
+
+    for student_times in permanence_period:
+        if is_integer(student_times) == False:
+            return None
+
+        if student_times[0] <= target_time <= student_times[1]:
+            sum += 1
+    return sum
+
+
+# print(study_schedule(permanence_period, 4))
