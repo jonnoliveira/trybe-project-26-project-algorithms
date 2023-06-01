6b642abc09d9e54ac620bb075c30fdc05b2ed16e
jonnoliveira <jonnutri@gmail.com>
2023-06-01 16:55:35 -0300

    Add: Requisito 2 - Palindromes_recursive

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,22 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    word = word.lower()  # converte para deixar todas as letras iguais
+
+    if len(word) == 0:  # verifica se a palavra é vazia
+        return False
+
+    if (
+        word[low_index] != word[high_index]
+    ):  # verifica se a primeira e a ultima letra são diferentes
+        return False
+
+    if (
+        low_index >= high_index
+    ):  # verifica se o indie da primeira letra é maior ou igual ao indice da ultima letra para saber se a palavra é palindroma
+        return True
+
+    return is_palindrome_recursive(
+        word, low_index + 1, high_index - 1
+    )  # chama a função novamente para verificar as proximas letras
+
+
+# print(is_palindrome_recursive("ana", 0, 2))
