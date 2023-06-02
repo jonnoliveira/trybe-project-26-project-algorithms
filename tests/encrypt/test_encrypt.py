8baaf32507c700dc29cdff915bafaaddc55901ba
jonnoliveira <jonnutri@gmail.com>
2023-06-01 22:07:48 -0300

    Add: Requisito 2 - Test_encrypt

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,20 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
+
+INVALID_KEY = "tipo inválido para key"
+INVALID_MESSAGE = "tipo inválido para message"
+MESSAGE = "Alohomora"
+ASSERTION_ONE = "aromoholA"
+ASSERION_TWO = "olA_aromoh"
+ASSERTION_THREE = "aromoho_lA"
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError, match=INVALID_KEY):
+        encrypt_message(MESSAGE, "1")
+    with pytest.raises(TypeError, match=INVALID_MESSAGE):
+        encrypt_message(777, 3)
+
+    assert encrypt_message(MESSAGE, 15) == ASSERTION_ONE
+    assert encrypt_message(MESSAGE, 3) == ASSERION_TWO
+    assert encrypt_message(MESSAGE, 2) == ASSERTION_THREE
